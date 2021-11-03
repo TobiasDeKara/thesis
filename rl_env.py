d449a3eaa83fddf15e6b68463d424e69d1db1934
Toby DeKara <tobias_dekara@brown.edu>
2021-11-03 16:22:41 -0400

    Implemented the 'BNBTree' class from l0bnb.

--- rl_env.py
+++ rl_env.py
@@ -8,28 +8,37 @@ import os
 import subprocess
 import numpy as np
 from l0bnb.node import Node
+from l0bnb import BNBTree as tree
 
-x_file_list = subprocess.run("cd synthetic_data; ls X* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
-y_file_list = subprocess.run("cd synthetic_data; ls y* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
-b_file_list = subprocess.run("cd synthetic_data; ls b* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
+# x_file_list = subprocess.run("cd synthetic_data; ls X* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
+# y_file_list = subprocess.run("cd synthetic_data; ls y* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
+# b_file_list = subprocess.run("cd synthetic_data; ls b* -1", capture_output=True, text=True, shell=True).stdout.splitlines()
 
 # load synthetic data
 x = np.load(os.path.join('synthetic_data', 'X_gen_syn_n3_p3_supp10_seed10002539.npy'))
 y = np.load(os.path.join('synthetic_data', 'y_gen_syn_n3_p3_supp10_seed10002539.npy'))
 
-# Create node_0
-p = x.shape[0]
-zlb = np.zeros(p, dtype='int32')
-zub = np.ones(p, dtype='int32')
-node_0 = Node(parent = None, zlb = zlb, zub = zub, x = x, y = y)
-node_0.xi_norm = np.linalg.norm(node_0.x, axis=0) ** 2
+t = tree(x, y)
+l0=1
+l2=1
+m=5
 
-# Solve relaxation
+# TODO: look into the following (from 'tree.solve') ...
+# "upper_bound, upper_beta, support = self._warm_start(warm_start, verbose, l0, l2, m)"
+
+# initialize root node (from 'tree.solve')
+t.root = Node(parent=None, zlb=[], zub=[], x=t.x, y=t.y, xi_norm=t.xi_norm)
+t.number_of_nodes = 1
+
+# Solve relaxation of root node
 # This returns the primal value and dual value, and it updates
 # self.primal_value, .dual_value, .primal_beta, .z, .support, .r, .gs_xtr, and .gs_xb
-# (I'm not sure what 'rel_tol' is, or what a good value is.)
-node_0.lower_solve(l0=0, l2=0, m=10, solver='l1cd', rel_tol=1, mio_gap=0)
+relax_sol = t.root.lower_solve(l0, l2, m, solver='l1cd', rel_tol=1e-4, mio_gap=0)
+# print(f'relax_sol: {relax_sol}')
 
 # TODO: turn this into a state space
-# TODO: handle branching
 
+
+
+
+# TODO: handle branching
