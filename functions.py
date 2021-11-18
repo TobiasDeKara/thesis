5e4bed30254a85decd06d64e005502bd0947baee
Toby DeKara <tobias_dekara@brown.edu>
2021-11-18 12:35:55 -0500

    Updated to use a dictionary instead of a list for 'active_nodes'.

--- functions.py
+++ functions.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 def get_active_node_stats(active_nodes):
-	len_zubs = [len(node.zub) for node in active_nodes]
-	len_zlbs = [len(node.zlb) for node in active_nodes]
+	len_zubs, len_zlbs, primal_values, len_supports = ([],)*4
+	for key in active_nodes:
+		node = active_nodes[key]
+		len_zubs.append(len(node.zub))
+		len_zlbs.append(len(node.zlb))
+		primal_values.append(node.primal_value)
+		len_supports.append(len(node.support))
 	len_zubs_percentiles = np.quantile(len_zubs,[0,0.25,0.5,0.75,1])
 	len_zlbs_percentiles = np.quantile(len_zlbs,[0,0.25,0.5,0.75,1])
-	primal_values = [node.primal_value for node in active_nodes]
 	primal_values_percentiles = np.quantile(primal_values,[0,0.25,0.5,0.75,1])
-	len_supports = [len(node.support) for node in active_nodes]
 	len_supports_percentiles = np.quantile(len_supports,[0,0.25,0.5,0.75,1])
 	stats = (len_zubs_percentiles, len_zlbs_percentiles,\
 		primal_values_percentiles, len_supports_percentiles)
@@ -27,12 +30,17 @@ def get_static_stats(cov, x, y, active_nodes, active_x_i, global_stats):
 		np.quantile(all_x_dot_y,[0,0.25,0.5,0.75,1]))
 
 	### Stats for all ACTIVE x
-	cov_active = cov[active_x_i,:][:,active_x_i]
-	q = cov_active.shape[0]
-	cov_active_flat = np.partition(cov_active.flatten(), -q)[:-q]
 	active_x_dot_y = all_x_dot_y[active_x_i]
-	active_x_stats = np.append(np.quantile(cov_active_flat,[0,0.25,0.5,0.75,1]), \
-		np.quantile(active_x_dot_y,[0,0.25,0.5,0.75,1]))
+	if len(active_x_i) > 1:
+		cov_active = cov[active_x_i,:][:,active_x_i]
+		q = cov_active.shape[0]
+		cov_active_flat = np.partition(cov_active.flatten(), -q)[:-q]
+		active_x_stats = np.append(np.quantile(cov_active_flat,[0,0.25,0.5,0.75,1]), \
+			np.quantile(active_x_dot_y,[0,0.25,0.5,0.75,1]))
+	else:
+		active_x_stats = np.append(np.zeros(5), \
+			np.quantile(active_x_dot_y,[0,0.25,0.5,0.75,1]))
+	
 	active_x_stats = np.append(active_x_stats, len(active_x_i))
 
 	### Stats for all ACTIVE nodes
@@ -46,8 +54,8 @@ def get_static_stats(cov, x, y, active_nodes, active_x_i, global_stats):
 def get_action_specific_stats(active_nodes, p, cov, x, y):
 	### Stats for node
 	all_action_specific_stats = []
-	for j in range(len(active_nodes)):
-		node = active_nodes[j]
+	for key in active_nodes:
+		node = active_nodes[key]
 		len_support = len(node.support) if node.support else 0
 		node_stats = np.array([len(node.zub), len(node.zlb), node.primal_value, len_support])
 
@@ -70,7 +78,7 @@ def get_action_specific_stats(active_nodes, p, cov, x, y):
 				beta = node.primal_beta[node.support.index(i)]
 			else:
 				beta = 0
-			x_i_node_stats = np.array([lb, ub, beta, i, j])
+			x_i_node_stats = np.array([lb, ub, beta, i, key])
 
 			### Collect action-specific stats
 			action_specific_arrays = [node_stats, x_i_stats, x_i_node_stats]
@@ -80,7 +88,13 @@ def get_action_specific_stats(active_nodes, p, cov, x, y):
 	all_action_specific_stats = np.vstack(all_action_specific_stats)
 	return(all_action_specific_stats)
 
-def int_sol(node, int_tol=10**-4, m=5):
+def int_sol(node, p, int_tol=10**-4, m=5):
+	# Simple verion used for testing
+	# if len(node.zlb + node.zub) == p:
+	#	return(True)
+	# else: 
+	#	return(False)
+
 	for i in node.support:
 		if i not in node.zlb and i not in node.zub:
 			# Note: the primal_beta values are indexed relative to len(support), 
@@ -92,6 +106,18 @@ def int_sol(node, int_tol=10**-4, m=5):
 				return(False)
 	return(True)
 
+	
+def prin(**kwargs):
+    for name, value in kwargs.items():
+        print(name,': ', value)
+
+
+
+
+
+
+
+		
 
 
 
@@ -100,4 +126,8 @@ def int_sol(node, int_tol=10**-4, m=5):
 
 
 
-			
+
+
+
+
+	
