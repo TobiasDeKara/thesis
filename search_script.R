802771d36ab905241250b1930bff234e4143b274
Toby DeKara <tobias_dekara@brown.edu>
2021-11-29 16:48:15 -0500

    This is a fully working version for calling the L0Learn cyclic coordinate descent routine. There's one outstanding TODO: setting intercept=False, rather than removing the intercept afterward.

--- search_script.R
+++ search_script.R
@@ -8,17 +8,15 @@
 # in the 'L0Learn' package these are referred to as 'lambda' and 'gamma'.
 
 library(L0Learn)
-options(tidyverse.quiet = TRUE)
-library(tidyverse)
+library(readr)
 
 x_sub <- read_csv('./param_for_search/x_sub.csv', col_names=TRUE, col_types='d')
-col_names <- names(x_sub) %>% as.integer() # these are the indexes of the variables relative to p
+col_names <- as.integer(names(x_sub)) # these are the indexes of the variables relative to p
 # Note that later 'coefs(fit, lambda, gamma)' will give us indexes of the support relative to x_sub
 
-x_sub <- x_sub %>% as.matrix() # L0Learn.fit needs a matrix
+x_sub <- as.matrix(x_sub) # L0Learn.fit needs a matrix
 
-y <- read_csv('./param_for_search/y.csv', col_names=FALSE, col_types='d') %>%
-	as.matrix()
+y <- as.matrix(read_csv('./param_for_search/y.csv', col_names=FALSE, col_types='d'))
 lambdas <- read_csv('./param_for_search/lambdas.csv', col_names=FALSE, col_types='d')
 lambda <- lambdas[[1,1]]
 gamma <- lambdas[[2,1]]
@@ -32,10 +30,10 @@ betas_unsorted <- coefs_with_intercept[-1,]
 index_of_support_in_x_sub <- coefs_with_intercept@i[-1]
 index_of_support_in_p_unsorted <- col_names[index_of_support_in_x_sub]
 
-betas <- betas_unsorted[order(index_of_support_in_p_unsorted)] %>% unname()
-support <- index_of_support_in_p_unsorted %>% sort()
+betas <- unname(betas_unsorted[order(index_of_support_in_p_unsorted)])
+support <- sort(index_of_support_in_p_unsorted)
 
-write.csv(betas, './results_of_search/betas.csv', row.names=FALSE)
-write.csv(support, './results_of_search/support.csv', row.names=FALSE)
+write.table(betas, './results_of_search/betas.csv', row.names=FALSE, col.names=FALSE)
+write.table(support, './results_of_search/support.csv', row.names=FALSE, col.names=FALSE)
 
 
