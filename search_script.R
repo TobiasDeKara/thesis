9c42ae43df51c0e0f1cf4bbd5e44daadb17ef6e2
Toby DeKara <tobias_dekara@brown.edu>
2022-01-19 16:13:06 -0500

    Updated for command line argument for epoch_n.

--- search_script.R
+++ search_script.R
@@ -10,17 +10,19 @@
 library(L0Learn)
 library(readr)
 
-x_sub <- read_csv('./param_for_search/x_sub_mat.csv', col_names=TRUE, show_col_types = FALSE)
+epoch_n = commandArgs(trailingOnly=TRUE)
+path <- paste0('./param_for_search/epoch_', epoch_n, '/')
+x_sub <- read_csv(paste0(path, 'x_sub_mat.csv'), col_names=TRUE, show_col_types = FALSE)
 col_names <- as.integer(names(x_sub)) # these are the indexes of the variables relative to p
 # Note that later 'coefs(fit, lambda, gamma)' will give us indexes of the support relative to x_sub
 
 x_sub <- as.matrix(x_sub) # L0Learn.fit needs a matrix
 
-y <- as.matrix(read_csv('./param_for_search/y.csv', col_names=FALSE, col_types='d', show_col_types = FALSE))
-lambdas <- read_csv('./param_for_search/lambdas.csv', col_names=FALSE, show_col_types = FALSE, col_types='d')
+y <- as.matrix(read_csv(paste0(path, 'y.csv'), col_names=FALSE, col_types='d', show_col_types = FALSE))
+lambdas <- read_csv(paste0(path, 'lambdas.csv'), col_names=FALSE, show_col_types = FALSE, col_types='d')
 lambda <- lambdas[[1,1]]
 gamma <- lambdas[[2,1]]
-len_zub <- read_csv('./param_for_search/len_zub.csv', col_names=FALSE, col_types='i', show_col_types = FALSE)
+len_zub <- read_csv(paste0(path, 'len_zub.csv'), col_names=FALSE, col_types='i', show_col_types = FALSE)
 len_zub <- len_zub[[1,1]]
 
 fit <- L0Learn.fit(x_sub, y, algorithm="CDPSI", penalty='L0L2', intercept=FALSE,
@@ -32,7 +34,8 @@ index_of_support_in_p_unsorted <- col_names[index_of_support_in_x_sub]
 betas <- unname(betas_unsorted[order(index_of_support_in_p_unsorted)])
 support <- sort(index_of_support_in_p_unsorted)
 
-write.table(betas, './results_of_search/betas.csv', row.names=FALSE, col.names=FALSE)
-write.table(support, './results_of_search/support.csv', row.names=FALSE, col.names=FALSE)
+path <- paste0('./results_of_search/epoch_', epoch_n, '/')
+write.table(betas, paste0(path, 'betas.csv'), row.names=FALSE, col.names=FALSE)
+write.table(support, paste0(path, 'support.csv'), row.names=FALSE, col.names=FALSE)
 
 
