f0fa195651d255a55c5453b74d7c3080c757d8b1
Toby DeKara <tobias_dekara@brown.edu>
2022-01-19 16:13:34 -0500

    Added get_validation_mse().

--- model_performance.py
+++ model_performance.py
@@ -1,6 +1,8 @@
 import numpy as np
 import subprocess
 import os
+import tensorflow as tf
+# Note: model_record format is at bottom for reference
 
 # Mean squared error
 def get_mse(epoch_n):
@@ -32,10 +34,58 @@ def get_mse(epoch_n):
       branch_mse = sum_sq / n_obs
       n_branches = n_obs
       
-    # np.quantile(q_hat, np.linspace(0, 1, 10))
-    
+    n_non_zero_obs = change_in_opt_gap
+
   return(f'branch_mse: {branch_mse}, search_mse: {search_mse}, \
   n_branches: {n_branches}, n_searches: {n_searches}')
+
+def get_validation_mse(epoch_n=1):
+    for i in range(2): 
+        sum_sq = 0
+        if i == 0:
+            # branch
+            branch_model_name = 'branch_model_in60_lay2'
+            branch_model = tf.keras.models.load_model(f'./models/{branch_model_name}')
+            branch_record_list = subprocess.run(f'cd action_records/epoch_{epoch_n}; ls branch* -1U', \
+            capture_output=True, text=True, shell=True).stdout.splitlines()
+
+            n_branches = 0
+            for branch_file_name in branch_record_list:
+                branch_record = np.load(f'./action_records/epoch_{epoch_n}/{branch_file_name}')
+                n_col = branch_record.shape[1]
+                x, y = np.hsplit(branch_record, np.array([n_col-1]))
+                batch_mse = branch_model.evaluate(x, y, verbose=0)
+                batch_n =  branch_record.shape[0]
+                sum_sq += batch_mse*batch_n
+                n_branches += batch_n
+                branch_mse = sum_sq / n_branches
+        else:
+            # Repeat for search
+            search_model_name='search_model_in51_lay2'
+            search_model = tf.keras.models.load_model(f'./models/{search_model_name}')
+            search_model_name = ''
+            search_record_list = subprocess.run(f'cd action_records/epoch_{epoch_n}; ls search* -1U', \
+            capture_output=True, text=True, shell=True).stdout.splitlines()
+
+            n_searches = 0
+            for search_file_name in search_record_list:
+                search_record = np.load(f'./action_records/epoch_{epoch_n}/{search_file_name}')
+                n_col = search_record.shape[1]
+                x, y = np.hsplit(search_record, np.array([n_col-1]))
+                batch_mse = search_model.evaluate(x, y, verbose=0)
+                batch_n = search_record.shape[0]
+                sum_sq += batch_mse*batch_n
+                n_searches += batch_n
+                search_mse = sum_sq / n_searches
+    return(f'branch_mse: {branch_mse}, search_mse: {search_mse}, \
+    n_branches: {n_branches}, n_searches: {n_searches}')
+
+    # model_rec_file_list = subprocess.run( \
+    # f"cd model_records/epoch_{epoch_n}; ls search_model_records* -1U", \
+    # capture_output=True, text=True, shell=True).stdout.splitlines()
+    
+    # model_rec_file_name = model_rec_file_list[0]
+    #  model_rec = np.load(os.path.join(f'model_records/epoch_{epoch_n}', model_rec_file_name))
 
 # For reference
 # model_record = np.array([
@@ -47,5 +97,3 @@ def get_mse(epoch_n):
 #     action.change_in_opt_gap, \
 #     model_epoch])
 
-# Mean Squared Error
-# def get_mse(model_rec_file_list):
