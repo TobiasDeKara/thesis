f7282d0d81259215a2d8c0ccac1740eb67bacabb
Toby DeKara <tobias_dekara@brown.edu>
2022-01-23 21:35:14 -0500

    Split these bc bug where it re-ran the q-mod initialization.

---
+++ init_q_mods.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# For Branhcing
+model = keras.Sequential()
+model.add( layers.Dense(4, activation="relu", input_shape=(60,)) ) # 46 static stats plus 14 branch-specific stats
+model.add(layers.Dense(1, activation="relu"))
+
+optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001
+model.compile(optimizer=optimizer, loss= "mean_squared_error")
+print(model.summary())
+model.save('./models/branch_model_in60_lay2')
+
+# For Searching
+model = keras.Sequential()
+model.add( layers.Dense(4, activation="relu", input_shape=(51,)) ) # 46 static stats plus 5 search-node-specific
+model.add(layers.Dense(1, activation="relu"))
+optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001
+model.compile(optimizer=optimizer, loss= "mean_squared_error")
+print(model.summary())
+model.save('./models/search_model_in51_lay2')
+
