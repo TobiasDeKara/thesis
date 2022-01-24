467bd1312274955e63b9ee90b05f86393bfef0ce
Toby DeKara <tobias_dekara@brown.edu>
2022-01-24 10:26:49 -0500

    Adjusted learning_rate to avoid all zero predictions

--- init_q_mods.py
+++ init_q_mods.py
@@ -4,21 +4,20 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 # For Branhcing
-model = keras.Sequential()
-model.add( layers.Dense(4, activation="relu", input_shape=(60,)) ) # 46 static stats plus 14 branch-specific stats
-model.add(layers.Dense(1, activation="relu"))
-
-optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001
-model.compile(optimizer=optimizer, loss= "mean_squared_error")
-print(model.summary())
-model.save('./models/branch_model_in60_lay2')
+branch_model = keras.Sequential()
+branch_model.add( layers.Dense(4, activation="relu", input_shape=(60,)) ) # 46 static stats plus 14 branch-specific stats
+branch_model.add(layers.Dense(1, activation="relu"))
+optimizer = keras.optimizers.Adam(learning_rate=0.01) # default learning rate == 0.001
+branch_model.compile(optimizer=optimizer, loss= "mean_squared_error")
+print(branch_model.summary())
+branch_model.save('./models/branch_model_in60_lay2')
 
 # For Searching
-model = keras.Sequential()
-model.add( layers.Dense(4, activation="relu", input_shape=(51,)) ) # 46 static stats plus 5 search-node-specific
-model.add(layers.Dense(1, activation="relu"))
-optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001
-model.compile(optimizer=optimizer, loss= "mean_squared_error")
-print(model.summary())
-model.save('./models/search_model_in51_lay2')
+search_model = keras.Sequential()
+search_model.add( layers.Dense(4, activation="relu", input_shape=(51,)) ) # 46 static stats plus 5 search-node-specific
+search_model.add(layers.Dense(1, activation="relu"))
+optimizer = keras.optimizers.Adam(learning_rate=0.01) # default learning rate == 0.001
+search_model.compile(optimizer=optimizer, loss= "mean_squared_error")
+print(search_model.summary())
+search_model.save('./models/search_model_in51_lay2')
 
