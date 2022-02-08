d01ffeb8f08281c83fa9142294290ab315bc89c2
Toby DeKara <tobias_dekara@brown.edu>
2022-02-08 16:30:55 -0500

    Single threded by batch for array job

--- gen_syn_data.py
+++ gen_syn_data.py
@@ -14,95 +14,10 @@ import sys
 import numpy as np
 from numpy.random import multivariate_normal, normal
 import os
-# import subprocess
-import multiprocessing
-   
-def make_6_batches(start_batch_n, run_n=int(sys.argv[1]), n_mat=10**3, n=10**3, 
-p=10**3, supp_size=10, rho=0.5, snr=5):
+import subprocess
 
-  seed_support_list = []
-
-  # Make batch sub-directories
-  for batch_n in range(start_batch_n, start_batch_n+6):
-    if p == 5:
-      out_dir = f'./synthetic_data/mini/batch_{batch_n}'
-    else: 
-      out_dir = f'./synthetic_data/p{int(np.log10(p))}/batch_{batch_n}'  
-    os.makedirs(out_dir, exist_ok=True)
-
-    for i in range(n_mat):
-      seed = seeds_subset[batch_n*n_mat + i]
-      np.random.seed(seed)
-     
-      # Make b
-      b = np.zeros((p,1))
-      support = np.random.choice(range(p), size=supp_size, replace=False)    
-      b[support] = np.ones((supp_size,1))
-
-      seed_support = np.concatenate([np.array(seed, ndmin=1), np.array(support, ndmin=1)])
-      seed_support_list.append(seed_support)
-
-      # Make x
-      cov_mat = np.zeros((p,p))
-      for row in range(p):
-        for col in range(p):
-          cov_mat[row, col] = rho**np.abs(row-col)
-        
-      x = multivariate_normal(mean=np.zeros(p), cov=cov_mat, size=n)
-      x_centered = x - np.mean(x, axis = 0)
-      x_normalized =  x_centered / np.linalg.norm(x_centered, axis = 0)
-      # Note that the norm in 'np.linalg.norm'(in the line above) defaults to L2
-        
-      # Make y
-      mu = np.matmul(x, b)
-      var_xb = (np.std(mu, ddof=1)) ** 2
-      sd_epsilon = np.sqrt(var_xb / snr)
-      epsilon = normal(size=n, scale=sd_epsilon)
-      y = mu + epsilon.reshape(-1,1)
-      y_centered = y - np.mean(y)
-      y_normalized = y_centered / np.linalg.norm(y_centered)
-
-      # Save x and y 
-      # Note: For brevity, the n and p values recorded in the file names are 
-      # the log base 10 of the actual values
-    
-      # Make file name
-      if p == 5:
-        filetag = f'gen_syn_n{int(np.log10(n))}_pmini_supp{supp_size}_seed{seed}' 
-      else:
-        filetag = f'gen_syn_n{int(np.log10(n))}_p{int(np.log10(p))}_supp{supp_size}_seed{seed}'  
-       
-      np.save(f'{out_dir}/x_{filetag}', x_normalized)
-      np.save(f'{out_dir}/y_{filetag}', y_normalized)
-      del x, y, b
-  
-  return(seed_support_list)
-
- 
-
-
-
-if __name__ == '__main__':
-  seeds = np.random.choice(10**8, 96*n_mat, replace=False)
-  first_batch_n=int(sys.argv[1])*96
-  start_batch_n_seq = range(first_batch_n, first_batch_n + 96, 6)
-  with multiprocessing.Pool() as pool:
-    pool.map(make_6_batches, start_batch_n_seq)
-# TODO: leaving off here
-    seed_support_array = np.vstack(seed_support_list)
-    np.save(f'{path}/seed_support_array_run_{run_n}', seed_support_array)
-
-
-# For Testing
-# import subprocess
-# x_file_list = subprocess.run( \
-# 		    	f"cd synthetic_data; ls x*_pmini_* -1U", \
-# 		    	capture_output=True, text=True, shell=True).stdout.splitlines()
-# x = x_file_list[3]
-# test = np.load(os.path.join(path, x))
-# test.shape
-# np.cov(test.T)
-
+def make_syn_data(n_mat=10**2, n=10**3, p=10**3, supp_size=10, rho=0.5, snr=5,\
+	 batch_n = int(sys.argv[1])):
     """Generate a synthetic regression dataset: y, x, and b.
     The data matrix x is sampled from a multivariate gaussian with exponential 
     correlation between columns.
@@ -124,5 +39,71 @@ if __name__ == '__main__':
         y: The response vector.
         b: The true vector of regression coefficients.
     """
+    # Create n_th batch sub-directory
+    if p == 5:
+        p_sub_dir = 'pmini'
+    else: 
+        p_sub_dir = f'p{int(np.log10(p))}'
 
+    xy_out_dir = f'~/thesis/synthetic_data/{p_sub_dir}/batch_{batch_n}'
+    os.makedirs(xy_out_dir, exist_ok=True)
 
+    seed_support_list = []
+    for _ in range(n_mat):
+        seed = int(10**9 * np.random.random_sample())
+        np.random.seed(seed)
+     
+        # Make b
+        b = np.zeros((p,1))
+        support = np.random.choice(range(p), size=supp_size)    
+        b[support] = np.ones(supp_size, 1)
+        seed_support = np.concatenate([np.array(seed, ndmin=1), np.array(support, ndmin=1)])
+        seed_support_list.append(seed_support)
+
+        # Make x
+        cov_mat = np.zeros((p,p))
+        for row in range(p):
+            for col in range(p):
+                cov_mat[row, col] = rho**np.abs(row-col)
+        
+        x = multivariate_normal(mean=np.zeros(p), cov=cov_mat, size=n)
+        x_centered = x - np.mean(x, axis = 0)
+        x_normalized =  x_centered / np.linalg.norm(x_centered, axis = 0)
+        # Note that the norm in 'np.linalg.norm'(in the line above) defaults to L2
+        
+        # Make y
+        mu = np.matmul(x, b)
+        var_xb = (np.std(mu, ddof=1)) ** 2
+        sd_epsilon = np.sqrt(var_xb / snr)
+        epsilon = normal(size=n, scale=sd_epsilon)
+        y = mu + epsilon.reshape(-1,1)
+        y_centered = y - np.mean(y)
+        y_normalized = y_centered / np.linalg.norm(y_centered)
+    
+        # Save
+        # Note: For brevity, the n and p values recorded in the file names are 
+        # the log base 10 of the actual values
+    
+        # Make file name
+        filetag = f'gen_syn_n{int(np.log10(n))}_{p_sub_dir}_supp{supp_size}_seed{seed}' 
+       
+        np.save(f'{xy_out_dir}/x_{filetag}', x_normalized)
+        np.save(f'{xy_out_dir}/y_{filetag)', y_normalized)
+        del x, y, b
+    
+    seed_support_array = np.vstack(seed_support_list)
+    b_out_dir = f'synthetic_data/{p_sub_dir}/seed_support_records_run_{run_n}'
+    os.makedirs(b_out_dir, exist_ok=True)
+    np.save(f'{b_out_dir}/seed_support_record_batch_{batch_n}', seed_support_array)
+
+make_syn_data(n_mat=10, p=1000, supp_size=10)
+
+# For Testing
+# import subprocess
+# x_file_list = subprocess.run( \
+# 		    	f"cd synthetic_data; ls x*_pmini_* -1U", \
+# 		    	capture_output=True, text=True, shell=True).stdout.splitlines()
+# x = x_file_list[3]
+# test = np.load(os.path.join(path, x))
+# test.shape
+# np.cov(test.T) 
