94bdb488929d701c61eeaa0b1735261dacab825b
Toby DeKara <tobias_dekara@brown.edu>
2022-02-16 21:18:17 -0500

    Added separate binary and numeric models

--- init_q_mods.py
+++ init_q_mods.py
@@ -2,27 +2,28 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+# from tensorflow.keras import activations
 
-branch = True
-search = True
+optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001 
 
-# For Branhcing
-if branch == True:
-	branch_model = keras.Sequential()
-	branch_model.add( layers.Dense(4, activation="relu", input_shape=(61,)) ) # 47 static stats plus 14 branch-specific stats
-	branch_model.add(layers.Dense(1, activation="relu"))
-	optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001 
-	branch_model.compile(optimizer=optimizer, loss= "mean_squared_error")
-	print(branch_model.summary())
-	branch_model.save('./models/branch_model_in61_lay2')
+for action_type in ['branch', 'search']:
+	if action_type == 'branch':
+		# Note on shape: 61 = 47 static stats plus 14 branch-specific stats
+		input_shape = 61
+	if action_type == 'search':
+		# Note on shape 53 = 47 static stats plus 6 search-node-specific
+		input_shape = 53
 
-# For Searching
-if search == True:
-	search_model = keras.Sequential()
-	search_model.add( layers.Dense(4, activation="relu", input_shape=(53,)) ) # 47 static stats plus 6 search-node-specific
-	search_model.add(layers.Dense(1, activation="relu"))
-	optimizer = keras.optimizers.Adam(learning_rate=0.001) # default learning rate == 0.001
-	search_model.compile(optimizer=optimizer, loss= "mean_squared_error")
-	print(search_model.summary())
-	search_model.save('./models/search_model_in53_lay2')
+	for reward_format in ['numeric', 'binary']:
+		model = keras.Sequential()
+		model.add( layers.Dense(4, activation="relu", input_shape=(input_shape,)) ) 
+		model.add(layers.Dense(1, activation="relu"))
+		if reward_format == 'binary':
+			model.add(layers.Dense(1, activation='sigmoid'))
+
+		model.compile(optimizer=optimizer, loss= "mean_squared_error")
+
+		print(model.summary())
+
+		model.save(f'./models/{action_type}_model_in{input_shape}_lay2_rew_{reward_format}')
 
