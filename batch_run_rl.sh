84ad385625e886f06bd3216c7d921f75ac2010e7
Toby DeKara <tobias_dekara@brown.edu>
2022-04-12 10:37:17 -0400

    Moved params inside batch scripts

--- batch_run_rl.sh
+++ batch_run_rl.sh
@@ -4,7 +4,7 @@
 # The memory usage is a function of the steps per ep
 # p = 50, 2.5 G max, ~400 steps per hour, working same episode for 8 hours
 # p = 50, 10k steps (100 ep x 100 steps per ep) 3 hours, less than 1G
-# p = 50, 81k steps (810 ep x 100 steps per ep) 3 hours, less than 1G
+# p = 50, 81k steps (810 ep total (90 per array id) x 100 steps per ep) 3 hours, less than 1G
 # p = 50, 30k steps (100 ep x 300 steps per ep) 13 hours, less than 1G
 # p = 100 10k steps (100 ep x 100 steps per ep) 5 hours
 # p = 1000 10k steps (100 ep x 100 steps per ep) 34 hours
@@ -14,7 +14,7 @@
 # --- Start of slurm commands -----------
 
 # Request an hour of runtime:
-#SBATCH --time=25:00:00
+#SBATCH --time=6:00:00 # test p50 6hrs, test p100 10hrs, test 1k ??
 
 ##SBATCH -p debug
 
@@ -25,10 +25,21 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias_dekara@brown.edu
 
-# Specify a job name:
-#SBATCH -J run4_1k_50
-
 #SBATCH --array=0-8  # 0- 3*n_batches-1
+
+# Specify a job name:
+#SBATCH -J te_50_bin_ran
+##SBATCH -J te_50_bb
+##SBATCH -J te_50_sig_num
+##SBATCH -J te_100_bin_ran
+##SBATCH -J te_100_bb
+##SBATCH -J te_100_sig_num
+##SBATCH -J te_1000_bin_ran
+##SBATCH -J te_1000_bb
+##SBATCH -J te_1000_sig_num
+##SBATCH -J te_100_bin_spec
+
+
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e ./sbatch_errors/%J_%a.err
@@ -40,21 +51,31 @@
 module load python/3.9.0
 source ~/L0_env_p3.9.0/bin/activate
 
-# train
-# python ~/thesis/run_rl.py    50    4    2       $SLURM_ARRAY_TASK_ID  250 train range binary
-# python ~/thesis/run_rl.py    100    4    2       $SLURM_ARRAY_TASK_ID  100 train range binary
-python ~/thesis/run_rl.py    1000    4    2       $SLURM_ARRAY_TASK_ID  50 train range binary
+### Train
+#  {p} {run_n} {batch_n} {L0_L2_n} {max_n_steps} {test_train} {model_scope} {reward_format}
 
-# test
-# python -u ~/thesis/run_rl.py    50    99    1   $SLURM_ARRAY_TASK_ID  100 test range binary
-# python -u ~/thesis/run_rl.py    50    99    1   $SLURM_ARRAY_TASK_ID  100 test range numeric
-# python -u ~/thesis/run_rl.py    50    99    1   $SLURM_ARRAY_TASK_ID  100 test range big_binary
-# python -u ~/thesis/run_rl.py    50    99    1   $SLURM_ARRAY_TASK_ID  100 test range sig_num
+# The following parmas were estimated to run in 10 hours
+# python ~/thesis/run_rl.py   50    4    2       $SLURM_ARRAY_TASK_ID  250 train range binary
+# python ~/thesis/run_rl.py  100    4    2       $SLURM_ARRAY_TASK_ID  100 train range binary
+# python ~/thesis/run_rl.py 1000    4    2       $SLURM_ARRAY_TASK_ID  50 train range binary
 
-# test specific models
-# python -u ~/thesis/run_rl.py    100    99    1        4     1000 test   specific  binary
+### Test
+python -u ~/thesis/run_rl.py   50    98    1   $SLURM_ARRAY_TASK_ID  50 test range binary
+# python -u ~/thesis/run_rl.py   50    98    1   $SLURM_ARRAY_TASK_ID  50 test range big_binary
+# python -u ~/thesis/run_rl.py   50    98    1   $SLURM_ARRAY_TASK_ID  50 test range sig_num
 
-#  {p} {run_n} {batch_n} {L0_L2_n}  {max_n_steps} {test_train} {model_scope} {reward_format}
+# python -u ~/thesis/run_rl.py   100   98    1   $SLURM_ARRAY_TASK_ID  100 test range binary
+# python -u ~/thesis/run_rl.py   100   98    1   $SLURM_ARRAY_TASK_ID  100 test range big_binary
+# python -u ~/thesis/run_rl.py   100   98    1   $SLURM_ARRAY_TASK_ID  100 test range sig_num
+
+# python -u ~/thesis/run_rl.py   1000  98    1   $SLURM_ARRAY_TASK_ID  1000 test range binary
+# python -u ~/thesis/run_rl.py   1000  98    1   $SLURM_ARRAY_TASK_ID  1000 test range big_binary
+# python -u ~/thesis/run_rl.py   1000  98    1   $SLURM_ARRAY_TASK_ID  1000 test range sig_num
+
+# python -u ~/thesis/run_rl.py    100    99    1        4              100 test specific  binary
+
+#  {p} {run_n} {batch_n} {L0_L2_n} {max_n_steps} {test_train} {model_scope} {reward_format}
+
 # batch_n: 0 for training, and 1 for testing
 # log_L0 = int(L0_L2_n/3) + 2
 # log_L2 = (L0_L2_n % 3) + 2    i.e. log_L0 and log_L2 each cycle through 2,3,4
--- batch_train_q.sh
+++ batch_train_q.sh
@@ -2,13 +2,17 @@
 
 # --- Start of slurm commands -----------
 # Run time notes:
+# Up to 10k epochs, early stopping patience 100, 10 hours
 # 300 epochs, 1:10, max 1G 
 
-#SBATCH --time=10:00:00
+#SBATCH --time=25:00:00
 #SBATCH --mem=3G 
 
 # Specify a job name:
-#SBATCH -J big_num_3
+#SBATCH -J tr_bin_ran
+##SBATCH -J tr_bb
+##SBATCH -J tr_sig
+##SBATCH -J tr_bin_spec
 
 # Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
 #SBATCH -e ./sbatch_errors/%J.err
@@ -22,10 +26,13 @@
 # Run commands
 module load python/3.9.0
 source ~/L0_env_p3.9.0/bin/activate
-python ~/thesis/train_q_models.py $1 6 yes True 1e-05 $2 $3 
-# run_n, model_scope, reward_format
 
-# <run_n> <n_layer> <drop_out>   <regularization> <learning_rate>        <model_scope>     reward_format
-# integer  integer   {yes, no}    {True, False}     1e-05   {range, specific} {big_binary, binary, numeric, sig_num}
+python ~/thesis/train_q_models.py 4 6 yes True 1e-05 range binary 
+# python ~/thesis/train_q_models.py 4 6 yes True 1e-05 range big_binary 
+# python ~/thesis/train_q_models.py 4 6 yes True 1e-05 range sig_num
+# python ~/thesis/train_q_models.py 4 6 yes True 1e-05 specific binary
+
+# <run_n> <n_layer> <drop_out> <regularization> <learning_rate> <model_scope>     reward_format
+# integer  integer   {yes, no} {True, False}     1e-05   {range, specific} {big_binary, binary, numeric, sig_num}
 # sbatch batch_train_q.sh 0 6 yes True 1e-05 True
 
